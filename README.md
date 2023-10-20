b766abd8f5fcbd3733e904eaf5678aa334d90479
SanthoshRagu97 <santhoshraguraman@gmail.com>
2023-10-20 16:03:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# yumEats
+ food delivery homepage
---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import Delivery from "./components/Delivery";
+import Featured from "./components/Featured";
+import TopNav from "./components/TopNav";
+import TopPicks from "./components/TopPicks";
+import Meal from "./components/Meal";
+import Categories from "./components/Categories";
+import NewsLetter from "./components/NewsLetter"
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+       <TopNav></TopNav>
+       <Featured></Featured>
+       <Delivery></Delivery>
+       <TopPicks></TopPicks>
+       <Meal></Meal>
+       <Categories></Categories>
+       <NewsLetter></NewsLetter>
+       <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
